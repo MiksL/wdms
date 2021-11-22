92aca82cdf1b3296da5e9ac26aed54138a8325a5
Miks <miks.lapsa01@gmail.com>
2021-11-22 14:12:53 +0200

    Updated warehouse pages

--- resources/views/warehouses.blade.php
+++ resources/views/warehouses.blade.php
@@ -1,13 +1,17 @@
 @section('title', 'Warehouses')
 <x-app-layout>
     <div class="w-full grid place-items-center">
-        <div class="w-4/6 grid grid-cols-2 place-items-center gap-4 my-4 mx-4">
+        <div class="w-4/6 grid grid-cols-2 place-items-center gap-5 my-4 mx-4 text-gray-500 dark:text-gray-200">
             @foreach ($warehouses as $warehouse)
-                <a href="{{ route('warehouses.show', $warehouse->id) }}" class="w-full border-2 border-gray-300 dark:border-gray-500 rounded-xl">
+                <a href="{{ route('warehouses.show', $warehouse->id) }}" class="w-full border-2 border-gray-300 dark:border-gray-500 rounded-xl 
+                    hover:text-gray-900 dark:hover:text-gray-400 hover:border-gray-900 dark:hover:border-gray-400 
+                    transform
+                    hover:scale-105
+                    transition duration-100"
+                >
                     <div class="mx-3 my-3">
-                        <div>{{ $warehouse->name }}</div>
+                        <div class="font-bold">{{ $warehouse->name }}</div>
                         <div class="my-2">{{ $warehouse->location }}</div>
-                        <div>test</div>
                     </div>
                 </a>
             @endforeach
--- resources/views/warehouses/show-warehouse.blade.php
+++ resources/views/warehouses/show-warehouse.blade.php
@@ -2,32 +2,32 @@
     <div class="w-full grid place-items-center">
         @foreach($warehouse as $warehouse)
             @section('title', "$warehouse->name")
-            <div class="w-4/5 grid place-items-left my-5">
+            <div class="w-4/5 grid place-items-left my-5 text-gray-500 dark:text-gray-300">
                 <div class="my-2 text-lg">{{ $warehouse->location }}</div>
             </div>
 
-            <div>
-                <div>Currently stocked products:</div>
-                <div class="w-full grid place-items-center dark:bg-background-800">
+            <div class="w-full grid place-items-center text-gray-500 dark:text-gray-300">
+                <div>Currently stocked products</div>
+                <div class="w-4/5 grid place-items-center dark:bg-background-800">
                     <table class="mt-4 w-10/12 text-center divide-y divide-gray-200 dark:divide-gray-600 shadow overflow-hidden sm:rounded-lg">
-                        <thead class="bg-gray-50 dark:bg-gray-700 text-xs uppercase h-6">
+                        <thead class="bg-gray-50 dark:bg-gray-700 text-xs uppercase h-6 w-full">
                             <tr>
-                                <th class="text-gray-500 dark:text-gray-300">@sortablelink('id', 'Product ID')</th>
-                                <th class="text-gray-500 dark:text-gray-300">@sortablelink('name', 'Name')</th>
-                                <th class="text-gray-500 dark:text-gray-300">@sortablelink('price', 'Price')</th>
-                                <th class="text-gray-500 dark:text-gray-300">@sortablelink('size', 'Size')</th>
-                                <th class="text-gray-500 dark:text-gray-300">@sortablelink('weight', 'Weight')</th>
-                                <th class="text-gray-500 dark:text-gray-300">@sortablelink('inStock', 'In Stock')</th>
+                                <th class="text-gray-500 dark:text-gray-300 w-1/6">@sortablelink('id', 'Product ID')</th>
+                                <th class="text-gray-500 dark:text-gray-300 w-1/6">@sortablelink('name', 'Name')</th>
+                                <th class="text-gray-500 dark:text-gray-300 w-1/6">@sortablelink('price', 'Price')</th>
+                                <th class="text-gray-500 dark:text-gray-300 w-1/6">@sortablelink('size', 'Size')</th>
+                                <th class="text-gray-500 dark:text-gray-300 w-1/6">@sortablelink('weight', 'Weight')</th>
+                                <th class="text-gray-500 dark:text-gray-300 w-1/6">@sortablelink('inStock', 'In Stock')</th>
                             </tr>
                         </thead>
                         <tbody class="divide-y divide-gray-200 dark:divide-gray-600">
                             @foreach ($warehouseStock as $product)
                                 <tr class="text-gray-900 dark:text-gray-300">
-                                    <td class="py-1">{{ $product->product_id }}</td>
+                                    <td class="py-1 w-1/6">{{ $product->product_id }}</td>
                                     <td class="py-1">{{ $product->name }}</td>
                                     <td class="py-1">{{ $product->price }}</td>
-                                    <td class="py-1">{{ $product->size }}</td>
-                                    <td class="py-1">{{ $product->weight }}</td>
+                                    <td class="py-1">{{ $product->size }} cm</td>
+                                    <td class="py-1">{{ $product->weight }} kg</td>
                                     <td class="py-1">{{ $product->product_count }}</td>
                                 </tr>
                             @endforeach
