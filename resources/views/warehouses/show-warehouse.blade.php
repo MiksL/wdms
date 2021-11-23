73a13ec88b2395070d6b34c550a39e2efe3b6460
Miks <miks.lapsa01@gmail.com>
2021-11-23 13:44:00 +0200

    Updated warehouse page, controller

--- resources/views/warehouses/show-warehouse.blade.php
+++ resources/views/warehouses/show-warehouse.blade.php
@@ -23,7 +23,7 @@
                         <tbody class="divide-y divide-gray-200 dark:divide-gray-600">
                             @foreach ($warehouseStock as $product)
                                 <tr class="text-gray-900 dark:text-gray-300">
-                                    <td class="py-1 w-1/6">{{ $product->product_id }}</td>
+                                    <td class="py-1">{{ $product->product_id }}</td>
                                     <td class="py-1">{{ $product->name }}</td>
                                     <td class="py-1">{{ $product->price }}</td>
                                     <td class="py-1">{{ $product->size }} cm</td>
@@ -33,8 +33,39 @@
                             @endforeach
                         </tbody>
                     </table>
-                    <div class="w-7/12 my-3">
-                    </div>
+                </div>
+
+                
+                <div class="mt-5">
+                    Recently moved products
+                </div>
+                <div class="w-4/5 grid place-items-center dark:bg-background-800">
+                    <table class="mt-4 w-10/12 text-center divide-y divide-gray-200 dark:divide-gray-600 shadow overflow-hidden sm:rounded-lg">
+                        <thead class="bg-gray-50 dark:bg-gray-700 text-xs uppercase h-6 w-full">
+                            <tr>
+                                <th class="text-gray-500 dark:text-gray-300 w-1/7">@sortablelink('id', 'Shipment ID')</th>
+                                <th class="text-gray-500 dark:text-gray-300 w-1/7">@sortablelink('destinationStoreName', 'Destination Store')</th>
+                                <th class="text-gray-500 dark:text-gray-300 w-1/7">@sortablelink('productID', 'Product ID')</th>
+                                <th class="text-gray-500 dark:text-gray-300 w-1/7">@sortablelink('name', 'Product Name')</th>
+                                <th class="text-gray-500 dark:text-gray-300 w-1/7">@sortablelink('price', 'Price')</th>
+                                <th class="text-gray-500 dark:text-gray-300 w-1/7">@sortablelink('weight', 'Weight')</th>
+                                <th class="text-gray-500 dark:text-gray-300 w-1/7">@sortablelink('shippedProductCount', 'Amount')</th>
+                            </tr>
+                        </thead>
+                        <tbody class="divide-y divide-gray-200 dark:divide-gray-600">
+                            @foreach ($recentlyMovedProducts as $product)
+                                <tr class="text-gray-900 dark:text-gray-300">
+                                    <td class="py-1">{{ $product->id }}</td>
+                                    <td class="py-1">{{ $product->store_name }}</td>
+                                    <td class="py-1">{{ $product->shipped_product_id }}</td>
+                                    <td class="py-1">{{ $product->name }}</td>
+                                    <td class="py-1">{{ $product->price }}</td>
+                                    <td class="py-1">{{ $product->weight }} kg</td>
+                                    <td class="py-1">{{ $product->shipped_product_count }}</td>
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
                 </div>
             </div>
         @endforeach
