2d470af6556ef362927d2af188d00039f2fed9aa
Miks <miks.lapsa01@gmail.com>
2021-12-05 23:05:36 +0200

    Further updates to routes

--- routes/web.php
+++ routes/web.php
@@ -17,16 +17,22 @@ Route::group(['middleware' => 'auth'], function () {
     Route::get('/', [\App\Http\Controllers\DashboardController::class, 'index'])->name('dashboard');
     Route::get('/products', [\App\Http\Controllers\ProductController::class, 'index'])->name('products');
     Route::get('/products/add-product', [\App\Http\Controllers\ProductController::class, 'create'])->name('products.add-product');
-    Route::post('/products/add-product', [\App\Http\Controllers\ProductController::class, 'store'])->name('product.store');
+    Route::post('/products/add-product', [\App\Http\Controllers\ProductController::class, 'store'])->name('products.store');
+    Route::get('/products/{id}/edit', [\App\Http\Controllers\ProductController::class, 'edit'])->name('products.edit');
+    Route::put('/products/{id}', [\App\Http\Controllers\ProductController::class, 'update'])->name('products.update');
 
     Route::get('/warehouses', [\App\Http\Controllers\WarehouseController::class, 'index'])->name('warehouses');
     Route::get('/warehouses/{id}', [\App\Http\Controllers\WarehouseController::class, 'show'])->name('warehouses.show');
     Route::get('/warehouses/add-warehouse', [\App\Http\Controllers\WarehouseController::class, 'create'])->name('warehouses.add-warehouse');
     Route::post('/warehouses/add-warehouse', [\App\Http\Controllers\WarehouseController::class, 'store'])->name('warehouses.store');
+    Route::get('/warehouses/{id}/edit', [\App\Http\Controllers\WarehouseController::class, 'edit'])->name('warehouses.edit');
+    Route::put('/warehouses/{id}', [\App\Http\Controllers\WarehouseController::class, 'update'])->name('warehouses.update');
 
     Route::get('/stores', [\App\Http\Controllers\StoreController::class, 'index'])->name('stores');
     Route::get('/stores/add-store', [\App\Http\Controllers\StoreController::class, 'create'])->name('stores.add-store');
     Route::post('/stores/add-store', [\App\Http\Controllers\StoreController::class, 'store'])->name('stores.store');
+    Route::get('/stores/{id}/edit', [\App\Http\Controllers\StoreController::class, 'edit'])->name('stores.edit');
+    Route::put('/stores/{id}', [\App\Http\Controllers\StoreController::class, 'update'])->name('stores.update');
 });
 
 require __DIR__.'/auth.php';
