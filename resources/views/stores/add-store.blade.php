6116e255cf457ae73d34906873bfbddd551d8b41
Miks <miks.lapsa01@gmail.com>
2021-12-06 23:13:38 +0200

    Updated view

--- resources/views/stores/add-store.blade.php
+++ resources/views/stores/add-store.blade.php
@@ -1,10 +1,14 @@
-@section('title', 'Add Store')
+@section('title', 'Add a new store')
 <x-app-layout>
     <div class="w-full grid place-items-center">
-        <form action="POST">
+        <form action="POST" class="w-3/5 grid place-items-left my-5">
             <input type="text" class="searchBox mt-3 w-1/5 rounded-xl bg-gray-50 dark:bg-gray-700 border-gray-500 text-gray-500 dark:text-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50">
             <input type="text" class="searchBox mt-3 w-1/5 rounded-xl bg-gray-50 dark:bg-gray-700 border-gray-500 text-gray-500 dark:text-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50">
             <input type="text" class="searchBox mt-3 w-1/5 rounded-xl bg-gray-50 dark:bg-gray-700 border-gray-500 text-gray-500 dark:text-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50">
         </form>
     </div>  
+    <!-- 
+        TODO maybe
+        have all the product pages as one
+    -->
 </x-app-layout>
