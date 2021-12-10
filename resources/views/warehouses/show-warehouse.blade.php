f1f70f4f9e3a0641ae5ea34a604db65b72df504f
Miks <miks.lapsa01@gmail.com>
2021-12-10 14:58:48 +0200

    Updated show-warehouse view, fixed absolute button positioning

--- resources/views/warehouses/show-warehouse.blade.php
+++ resources/views/warehouses/show-warehouse.blade.php
@@ -1,6 +1,6 @@
 <x-app-layout>
     <div class="w-full grid place-items-center" x-data="{ selectedTable: 0 }">
-        <div class="absolute grid align-middle z-10 right-0 mt-5 text-gray-500 dark:text-gray-200 bg-gray-200 dark:bg-gray-900 rounded-l-xl">
+        <div class="absolute grid top-1/4 z-10 right-0 mt-5 text-gray-500 dark:text-gray-200 bg-gray-200 dark:bg-gray-900 rounded-l-xl">
             <button class="m-4 hover:text-gray-900 dark:hover:text-gray-400" @click="selectedTable = 0">
             Currently Stocked
             </button>
@@ -13,7 +13,7 @@
         </div>
         @foreach($warehouse as $warehouse)
             @section('title', "$warehouse->name")
-            <div class="w-4/5 grid place-items-left my-5 text-gray-500 dark:text-gray-300">
+            <div class="w-full grid place-items-center my-5 text-gray-500 dark:text-gray-300">
                 <div class="my-2 text-lg">{{ $warehouse->location }}</div>
             </div>
 
@@ -31,7 +31,7 @@
                 </div>
 
                 <div x-cloak x-show="selectedTable === 2">
-                    Supplied stores by the warehouse:
+                    Stores supplied by the warehouse:
                 </div>
                 <div x-cloak x-show="selectedTable === 2" class="w-full grid place-items-center">
                     @livewire('supplied-stores', ['warehouseid' => $warehouse->id])
