700568038b51fb754f31f7435e44aae46e7e8931
Miks <miks.lapsa01@gmail.com>
2022-06-11 16:06:04 +0300

    Updated dark mode toggle design

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -24,8 +24,8 @@
                     <div class="relative rounded-full w-12 h-6 transition duration-200 ease-linear"
                         :class="[darkMode === true ? 'bg-gray-700' : 'bg-yellow-300']">
                         <label for="toggle"
-                                class="absolute left-0 bg-white border-2 mb-2 w-6 h-6 rounded-full transition transform duration-100 ease-linear cursor-pointer"
-                                :class="[ darkMode === true ? 'translate-x-full border-gray-400' : 'translate-x-0 border-yellow-400']">
+                                class="absolute left-0 border-2 border-yellow-400 mb-2 w-6 h-6 rounded-full transition transform duration-100 ease-linear cursor-pointer"
+                                :class="[ darkMode === true ? 'translate-x-full bg-gray-800' : 'translate-x-0 bg-white']">
                             <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-yellow-400" viewBox="0 0 20 20" fill="currentColor" x-show="!darkMode">
                                 <path fill-rule="evenodd" d="M10 2a1 1 0 011 1v1a1 1 0 11-2 0V3a1 1 0 011-1zm4 8a4 4 0 11-8 0 4 4 0 018 0zm-.464 4.95l.707.707a1 1 0 001.414-1.414l-.707-.707a1 1 0 00-1.414 1.414zm2.12-10.607a1 1 0 010 1.414l-.706.707a1 1 0 11-1.414-1.414l.707-.707a1 1 0 011.414 0zM17 11a1 1 0 100-2h-1a1 1 0 100 2h1zm-7 4a1 1 0 011 1v1a1 1 0 11-2 0v-1a1 1 0 011-1zM5.05 6.464A1 1 0 106.465 5.05l-.708-.707a1 1 0 00-1.414 1.414l.707.707zm1.414 8.486l-.707.707a1 1 0 01-1.414-1.414l.707-.707a1 1 0 011.414 1.414zM4 11a1 1 0 100-2H3a1 1 0 000 2h1z" clip-rule="evenodd"></path>
                             </svg>
