58695d720e8e0862c57af9997d29636c8acac801
Miks <miks.lapsa01@gmail.com>
2022-06-14 16:36:36 +0300

    Updated navbar
    - Removed unnecessary code from navbar
    - Improved navbar responsiveness
    - Added better navbar functionality at smaller screen sizes

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,25 +1,25 @@
-<div class="relative bg-white dark:bg-gray-800 dark:text-gray-200">
-    <div class="max-w-7xl mx-auto px-4 sm:px-6">
-        <div class="flex justify-between items-center border-b-2 border-gray-300 dark:border-gray-500 py-6 md:justify-start md:space-x-10">
+<div class="relative bg-white dark:bg-gray-800 text-gray-500 dark:text-gray-200">
+    <div class="max-w-7xl mx-auto px-6">
+        <div class="flex justify-between items-center border-b-2 border-gray-300 dark:border-gray-500 space-x-10 py-3 md:py-4 xl:py-6 md:justify-start">
             <div class="flex justify-start lg:w-0 lg:flex-1">
-                <a href="{{ route('dashboard') }}" class="text-base text-gray-500 dark:text-gray-200 hover:text-gray-900 dark:hover:text-gray-400 font-semibold text-xl leading-tight">
+                <a href="{{ route('dashboard') }}" class="text-base md:text-xl hover:text-gray-900 dark:hover:text-gray-400 font-semibold leading-tight">
                     @yield('title')
                 </a>
             </div>
-            <nav class="hidden md:flex space-x-10">
-                <a href="{{ route('warehouses') }}" class="text-base font-medium text-gray-500 dark:text-gray-200 hover:text-gray-900 dark:hover:text-gray-400">
+            <nav class="flex space-x-10 text-sm md:text-base font-medium">
+                <a href="{{ route('warehouses') }}" class="hover:text-gray-900 dark:hover:text-gray-400">
                     Warehouses
                 </a>
                 
-                <a href="{{ route('stores') }}" class="text-base font-medium text-gray-500 dark:text-gray-200 hover:text-gray-900 dark:hover:text-gray-400">
+                <a href="{{ route('stores') }}" class="hover:text-gray-900 dark:hover:text-gray-400">
                     Stores
                 </a>
 
-                <a href="{{ route('products') }}" class="text-base font-medium text-gray-500 dark:text-gray-200 hover:text-gray-900 dark:hover:text-gray-400">
+                <a href="{{ route('products') }}" class="hover:text-gray-900 dark:hover:text-gray-400">
                     Products
                 </a>
             </nav>
-            <div class="hidden md:flex items-center justify-end md:flex-1 lg:w-0">
+            <div class="flex items-center justify-end flex-1 lg:w-0">
                 <div class="flex justify-center items-center">
                     <div class="relative rounded-full w-12 h-6 transition duration-200 ease-linear"
                         :class="[darkMode === true ? 'bg-gray-700' : 'bg-yellow-300']">
@@ -40,22 +40,31 @@
                     </div>
                 </div>
                 <div @click.away="open = false" class="relative" x-data="{ open: false }">
-                    <button @click="open = !open" class="flex flex-row items-center w-full px-4 py-2 mt-2 text-sm font-medium text-left bg-transparent rounded-lg text-gray-500 dark:text-gray-200 dark:bg-transparent dark:focus:text-gray-400 dark:hover:text-gray-400 md:w-auto md:inline md:mt-0 md:ml-4 hover:text-gray-900 focus:text-gray-900 focus:outline-none focus:shadow-outline">
-                    <span>{{ Auth::user()->name }}</span>
-                    <svg fill="currentColor" viewBox="0 0 20 20" :class="{'rotate-180': open, 'rotate-0': !open}" class="inline w-4 h-4 mt-1 ml-1 transition-transform duration-200 transform md:-mt-1"><path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+                    <button @click="open = !open" class="flex flex-row items-center w-full px-4 py-2 mt-2 text-sm font-medium text-left bg-transparent rounded-lg dark:bg-transparent dark:focus:text-gray-400 dark:hover:text-gray-400 md:w-auto md:inline md:mt-0 md:ml-4 hover:text-gray-900 focus:text-gray-900 focus:outline-none focus:shadow-outline">
+                        <span>{{ Auth::user()->name }}</span>
+                        <svg fill="currentColor" viewBox="0 0 20 20" :class="{'rotate-180': open, 'rotate-0': !open}" class="inline w-4 h-4 mt-1 ml-1 transition-transform duration-200 transform md:-mt-1"><path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
                     </button>
-                    <div x-show="open" x-transition:enter="transition ease-out duration-100" x-transition:enter-start="transform opacity-0 scale-95" x-transition:enter-end="transform opacity-100 scale-100" x-transition:leave="transition ease-in duration-75" x-transition:leave-start="transform opacity-100 scale-100" x-transition:leave-end="transform opacity-0 scale-95" class="absolute right-0 w-full mt-2 origin-top-right rounded-md shadow-lg md:w-48">
-                    <div class="px-2 py-2 bg-white dark:bg-gray-800 rounded-md shadow">
-                        <form method="POST" action="{{ route('logout') }}">
-                            @csrf
-                            <x-dropdown-link class="block px-4 py-2 mt-2 text-sm font-medium bg-transparent rounded-lg dark:bg-transparent text-gray-500 dark:focus:text-gray-200 dark:hover:text-gray-400 dark:text-gray-200 md:mt-0 hover:text-gray-900 focus:text-gray-900 focus:outline-none focus:shadow-outline"
-                            :href="route('logout')"
-                                    onclick="event.preventDefault();
-                                                this.closest('form').submit();">
-                                {{ __('Log Out') }}
-                            </x-dropdown-link>
-                        </form>
-                    </div>
+                    <div x-show="open" 
+                    x-transition:enter="transition ease-out duration-100" 
+                    x-transition:enter-start="transform opacity-0 scale-95" 
+                    x-transition:enter-end="transform opacity-100 scale-100" 
+                    x-transition:leave="transition ease-in duration-75" 
+                    x-transition:leave-start="transform opacity-100 scale-100" 
+                    x-transition:leave-end="transform opacity-0 scale-95" 
+                    class="absolute right-0 w-full mt-2 origin-top-right rounded-md shadow-lg md:w-48">
+                        <div class="px-2 py-2 bg-white dark:bg-gray-800 rounded-md shadow">
+                            <form method="POST" action="{{ route('logout') }}">
+                                @csrf
+                                <x-dropdown-link class="block px-4 py-2 mt-2 md:mt-0 text-sm font-medium bg-transparent rounded-lg 
+                                dark:bg-transparent dark:text-gray-300 dark:focus:text-gray-200 dark:hover:text-gray-400
+                                hover:text-gray-900 focus:text-gray-900 focus:outline-none focus:shadow-outline"
+                                :href="route('logout')"
+                                        onclick="event.preventDefault();
+                                                    this.closest('form').submit();">
+                                    {{ __('Log Out') }}
+                                </x-dropdown-link>
+                            </form>
+                        </div>
                     </div>
                 </div>
             </div>
